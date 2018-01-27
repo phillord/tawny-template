2b2352c745c5141039371d3db937735fe11b6702
Phillip Lord <phillip.lord@russet.org.uk>
2018-01-27 11:27:41 +0000

    Allow projects to have nested directories.

--- src/leiningen/new/ontology.clj
+++ src/leiningen/new/ontology.clj
@@ -1,5 +1,6 @@
 (ns leiningen.new.ontology
   (:require
+   [clojure.string]
    [leiningen.new.templates :refer [renderer year date project-name
                                     ->files sanitize-ns name-to-path
                                     multi-segment
@@ -9,22 +10,25 @@
 (def render (renderer "ontology"))
 
 (defn ontology
-  "FIXME: write documentation"
+  "Generate a new project using Tawny-OWL."
   [name]
-  (println "name is" name)
   (let [
         core-ns (multi-segment (sanitize-ns name))
         main-ns (multi-segment (sanitize-ns name) name)
+        base-dir-path (name-to-path name)
+        onto-name
+        (last (clojure.string/split base-dir-path #"/"))
         data {:name name
               :namespace-main core-ns
               :namespace main-ns
-              :sanitized (name-to-path name)
+              :sanitized base-dir-path
+              :onto-name onto-name
               }]
     (main/info "Generating fresh 'lein new' ontology project.")
     (->files data
              ["project.clj" (render "project.clj" data)]
              ["src/{{sanitized}}/core.clj" (render "core.clj" data)]
-             ["src/{{sanitized}}/{{sanitized}}.clj"
+             ["src/{{sanitized}}/{{onto-name}}.clj"
               (render "onto.clj" data)]
-             ["test/{{sanitized}}/{{sanitized}}_test.clj"
+             ["test/{{sanitized}}/{{onto-name}}_test.clj"
               (render "onto_test.clj" data)])))
