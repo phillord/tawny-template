2608e1345c7c41ce787808ab9297dbec185b121b
Phillip Lord <phillip.lord@russet.org.uk>
2018-01-27 11:27:22 +0000

    Add nightlight profile for easy editing.

--- resources/leiningen/new/ontology/project.clj
+++ resources/leiningen/new/ontology/project.clj
@@ -2,4 +2,7 @@
   :description "An ontology for {{name}}"
   :dependencies [[uk.org.russet/tawny-owl "2.0.0-SNAPSHOT"]]
   :main {{namespace-main}}
+
+  :profiles
+  {:light {:plugins [[nightlight/lein-nightlight "1.9.0"]]}}
   )
